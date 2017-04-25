6c38479a939b096d79782f5c467f1aa22258cc06
Florian Noack <noack@fortiss.org>
2017-04-25 12:43:37 +0200

    Added pom.xml for maven support and refactored the project structure

--- src/main/java/app/Controller.java
+++ src/main/java/app/Controller.java
@@ -1,4 +1,4 @@
-package main.java.app;
+package app;
 
 public class Controller {
 }
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,4 @@
-package main.java.app;
+package app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../resources/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/mainFrame.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
