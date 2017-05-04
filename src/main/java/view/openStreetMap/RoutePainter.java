08a11d9f147b0a09f6f2e6bab97125e8f4ebf091
Florian Noack <noack@fortiss.org>
2017-05-04 11:01:47 +0200

    Added GrasHopper to Maven

--- src/main/java/view/openStreetMap/RoutePainter.java
+++ src/main/java/view/openStreetMap/RoutePainter.java
@@ -1,7 +1,7 @@
 package view.openStreetMap;
 
 /**
- * Created by flori on 18.04.2017.
+ * Created by Florian Noack on 18.04.2017.
  */
 
 import org.jxmapviewer.JXMapViewer;
@@ -20,81 +20,38 @@ public class RoutePainter implements Painter<JXMapViewer>
 {
 
     private Color color = Color.RED;
-
     private boolean antiAlias = true;
-
-
-
     private List<GeoPosition> track;
 
-
-
     /**
-
      * @param track the track
 
      */
-
-    public RoutePainter(List<GeoPosition> track)
-
-    {
-
+    public RoutePainter(List<GeoPosition> track){
         // copy the list so that changes in the
-
         // original list do not have an effect here
-
         this.track = new ArrayList<GeoPosition>(track);
-
     }
 
 
 
-    public void paint(Graphics2D g, JXMapViewer map, int w, int h)
-
-    {
-
+    public void paint(Graphics2D g, JXMapViewer map, int w, int h){
         g = (Graphics2D) g.create();
-
-
-
         // convert from viewport to world bitmap
-
         Rectangle rect = map.getViewportBounds();
-
         g.translate(-rect.x, -rect.y);
-
-
-
         if (antiAlias)
-
             g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-
-
         // do the drawing
-
         g.setColor(Color.BLACK);
-
         g.setStroke(new BasicStroke(4));
-
-
-
         drawRoute(g, map);
-
-
-
         // do the drawing again
 
         g.setColor(color);
-
         g.setStroke(new BasicStroke(2));
-
-
-
         drawRoute(g, map);
-
-
-
         g.dispose();
 
     }
@@ -102,61 +59,26 @@ public class RoutePainter implements Painter<JXMapViewer>
 
 
     /**
-
      * @param g the graphics object
-
      * @param map the map
-
      */
 
-    private void drawRoute(Graphics2D g, JXMapViewer map)
-
-    {
+    private void drawRoute(Graphics2D g, JXMapViewer map){
 
         int lastX = 0;
-
         int lastY = 0;
-
-
-
         boolean first = true;
-
-
-
-        for (GeoPosition gp : track)
-
-        {
-
+        for (GeoPosition gp : track){
             // convert geo-coordinate to world bitmap pixel
-
             Point2D pt = map.getTileFactory().geoToPixel(gp, map.getZoom());
-
-
-
-            if (first)
-
-            {
-
+            if (first){
                 first = false;
-
             }
-
-            else
-
-            {
-
+            else{
                 g.drawLine(lastX, lastY, (int) pt.getX(), (int) pt.getY());
-
             }
-
-
-
             lastX = (int) pt.getX();
-
             lastY = (int) pt.getY();
-
         }
-
     }
-
 }
